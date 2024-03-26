f7da47fc69d6b6f0e323c092d2118b10aea90898
alorenzot <alejandrolorenzotoledo@gmail.com>
2024-03-26 17:06:40 +0100

    commit

--- app/src/main/java/com/example/spinnerexample/MainActivity.java
+++ app/src/main/java/com/example/spinnerexample/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.spinnerexample;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner simple_spinner;
     private Spinner custom_spinner;
+    private Spinner string_array_spinner;
+    private TextView texto3;
+    private ImageView image2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +25,36 @@ public class MainActivity extends AppCompatActivity {
 
         simple_spinner = findViewById(R.id.simple_spinner);
         custom_spinner = findViewById(R.id.custom_spinner);
+        texto3=findViewById(R.id.text3);
+        image2=findViewById(R.id.image2);
+        string_array_spinner=findViewById(R.id.arraySpinner);
 
         ArrayAdapter<Sexo> arrayAdapter =
                 new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,Sexo.values());
         ArrayAdapter<Sexo> customArrayAdapter =
                 new CustomArrayAdapter<>(this, android.R.layout.simple_spinner_item,Sexo.values());
+        ArrayAdapter<CharSequence> stringArrayAdapter = ArrayAdapter.createFromResource(this,R.array.valores_array, android.R.layout.simple_spinner_item);
 
         simple_spinner.setAdapter(arrayAdapter);
         custom_spinner.setAdapter(customArrayAdapter);
+        string_array_spinner.setAdapter(stringArrayAdapter);
+
+        simple_spinner.setOnItemSelectedListener(this);
+        custom_spinner.setOnItemSelectedListener(this);
+
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Listable item = (Listable) adapterView.getItemAtPosition(i);
+        texto3.setText(item.getDescripcion());
+        image2.setImageResource(item.getIcon());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
     }
 
